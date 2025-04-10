a175b9f966f41756bc0516ffb6545932780f2680
Joydeb Biswas <163167613+JoydebGIThub@users.noreply.github.com>
2025-04-10 06:19:28 +0000

    conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+let val ="44abc"
+
+console.log(typeof score);
+console.log(typeof(val));
+
+let valueToNumber = Number(val);
+console.log(typeof valueToNumber);
+console.log(valueToNumber);
+
+let isLoggedIn = 1;
+let booleanIsLogged = Boolean(isLoggedIn);
+console.log(booleanIsLogged);
+
+// 1 => true; 0=> false
+//"" => false
+//"Joydeb" => true
+
+let numbers= 1232
+let stringNumber = String(numbers);
+console.log(typeof (stringNumber));
+
+
+
