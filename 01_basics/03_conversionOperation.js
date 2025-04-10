2c32c6fe6f67daade4978af46628e37c3cd30768
Joydeb Biswas <163167613+JoydebGIThub@users.noreply.github.com>
2025-04-10 06:35:01 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,16 @@
 let score = 33
 let val ="44abc"
 
-console.log(typeof score);
-console.log(typeof(val));
+// console.log(typeof score);
+// console.log(typeof(val));
 
 let valueToNumber = Number(val);
-console.log(typeof valueToNumber);
-console.log(valueToNumber);
+// console.log(typeof valueToNumber);
+// console.log(valueToNumber);
 
 let isLoggedIn = 1;
 let booleanIsLogged = Boolean(isLoggedIn);
-console.log(booleanIsLogged);
+// console.log(booleanIsLogged);
 
 // 1 => true; 0=> false
 //"" => false
@@ -18,7 +18,38 @@ console.log(booleanIsLogged);
 
 let numbers= 1232
 let stringNumber = String(numbers);
-console.log(typeof (stringNumber));
+// console.log(typeof (stringNumber));
+ //**************************** Operations************************************** */
+
+ let value= 3
+ let negValue= -value
+// console.log(negValue);
+// console.log(2**3);
+// console.log(2*3);
+
+let str1="hello";
+let str2=" Joydeb"
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 +"2");
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+
+let gameCounter= 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
 
 
 
