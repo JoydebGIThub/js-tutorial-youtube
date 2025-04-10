93332224cce53f3a04aa050d488d1f4f781502bb
Joydeb Biswas <163167613+JoydebGIThub@users.noreply.github.com>
2025-04-10 06:00:01 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const  accountId= 1225533
+let account_Email= "royr544@google.com"
+var account_password="1234q2"
+accountCity= "Kolkata"
+
+//accountId=2
+account_Email= "royr54gle.com"
+account_password="34q2"
+accountCity="Bangulor"
+console.log(accountId);
+console.table([accountId, account_Email, account_password, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) // we use nodejs not browser
+
+let name="Joydeb"
+let age= 27
+let isLoggedIn= false
+
+console.table([typeof "Joydeb", typeof age, typeof null, typeof undefined])
