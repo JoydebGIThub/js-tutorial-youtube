761308be05cf6f7b377d467a6f08a8b7a6fa4b33
Joydeb Biswas <163167613+JoydebGIThub@users.noreply.github.com>
2025-04-10 10:29:17 +0000

    number and math

---
+++ 01_basics/06_nums_andmaths.js
@@ -0,0 +1,34 @@
+const score=400
+// console.log(score);//400
+
+const balance= new Number(56)
+// console.log(balance)//[Number: 56]
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber= 23.49223;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds= 1000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//-------------------Maths------------------------
+console.log(Math.abs(-4)); // make negatives positive
+console.log(Math.round(4.6)); // 5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); //4
+console.log(Math.min(4, 3,2 ,4 )); //2
+console.log(Math.max(3, 2,4, 5,63, 2)); //63
+
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
+
+
+
