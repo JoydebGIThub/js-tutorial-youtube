5f85bee7c47b6af549b0e981fd44e12d1c3b64a6
Joydeb Biswas <163167613+JoydebGIThub@users.noreply.github.com>
2025-04-10 06:50:58 +0000

    comparasion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>3);
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); //false convert null to 0
+console.log(null == 0); //false
+console.log(null >= 0); //true convert null to 0
+
+//=== strick comparasion 
+
+console.log("2" === 2);//false
+console.log("2" == 2);//true
+
+
+
+
+
+
